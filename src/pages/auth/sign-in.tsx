c33daf710457cc1f8ecddb83134629c4124c1f8a
devjpedro <joaopedross.ti9@gmail.com>
2024-03-25 21:32:32 -0300

    react-helmet (seo)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <>
+      <Helmet title="Login" />
+      <div>
+        <h1>SignIn</h1>
+      </div>
+    </>
   )
 }
