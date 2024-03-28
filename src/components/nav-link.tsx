cea09610b416b5d6e045fb4cf7950a49b90f99c6
devjpedro <joaopedross.ti9@gmail.com>
2024-03-28 19:19:46 -0300

    themme toggle

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,13 +1,13 @@
 import { Link, LinkProps, useLocation } from 'react-router-dom'
 export type NavLinkProps = LinkProps
 
-export default function NavLink(props: NavLinkProps) {
+export function NavLink(props: NavLinkProps) {
   const { pathname } = useLocation()
 
   return (
     <Link
       data-current={pathname === props.to}
-      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground"
+      className="flex items-center gap-1.5 rounded p-1 text-sm font-medium text-muted-foreground ring-offset-background transition-colors hover:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[current=true]:text-foreground"
       {...props}
     />
   )
