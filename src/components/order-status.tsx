ccff24108e2c9ec63800d6998d59a0fb25bab647
devjpedro <joaopedross.ti9@gmail.com>
2024-04-02 20:08:49 -0300

    Operações no status do pedido

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,4 +1,4 @@
-type OrderStatus =
+export type OrderStatus =
   | 'pending'
   | 'canceled'
   | 'processing'
@@ -13,7 +13,7 @@ const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
   canceled: 'Cancelado',
   delivered: 'Entregue',
-  delivering: 'Em entraga',
+  delivering: 'Em entrega',
   processing: 'Em preparo',
 }
 
